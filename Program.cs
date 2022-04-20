e833214955c8cb0c8fe326263b697d41a6c1878d
biandades <bianda.destin@neon.com.br>
2022-04-20 18:08:19 -0300

    Subindo minha calculadora IMC

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CalculatorIMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.Clear();
+
+            Console.WriteLine("Peso em KG:");
+            float peso = float.Parse(Console.ReadLine());
+    
+            Console.WriteLine("Altura em Metro:");
+            float altura = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = peso / (altura * altura);
+            // Console.WriteLine($"O resultado do IMC é {resultado}");
+            Console.WriteLine("O resultado do IMC é " + resultado.ToString("F2")); //Limitando a 2 decimais
+
+            //Comparações de IMC --- Pedir sugestão para esse comentário
+            if(resultado <= 18.49)
+                Console.WriteLine("Você está abaixo do peso. Cuidado!!!");
+
+            if(resultado >= 18.5 && resultado < 24.9)
+                Console.WriteLine("Você está com peso normal. Parabéns!!!");
+            
+            if(resultado >= 25)
+                Console.WriteLine("Você está acima do peso. Cuidado!!!");
+        }
+    }    
+}
